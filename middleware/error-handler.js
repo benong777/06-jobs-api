3568b2b5bcb493b10fd9c9eaf9d42eefb6863b03
Ben <ong.ben@gmail.com>
2025-09-24 16:56:46 -0700

    Updated custom errors (ex: when email has already been used)

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,24 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customError = {
+    //-- Set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong. Try again later.',
+  };
+
+  //-- No longer needed since we added the error code checks below
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for the ${Object.keys(err.keyValue)} field, please choose another value`;
+    customError.statusCode = 400;
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
