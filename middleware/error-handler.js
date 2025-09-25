3502ab166556064547b914ebb6e214552bd21489
Ben <ong.ben@gmail.com>
2025-09-25 00:03:02 -0700

    Added better error handling responses

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -13,11 +13,26 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   //   return res.status(err.statusCode).json({ msg: err.message })
   // }
 
+  //-- Validation errors (missing email or password)
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join('. ');
+    customError.statusCode = 400;
+  }
+
+  //-- Duplicate (email only?) errors
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for the ${Object.keys(err.keyValue)} field, please choose another value`;
     customError.statusCode = 400;
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
+  //-- Cast errors (ex: wrong jobId)
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}.`
+    customError.statusCode = 404;
+  }
+
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
